37705f8c83bf8a782466e550c6b77acf5c298532
Mabroor Ahmed <mabroorahmed18@gmail.com>
2023-06-26 01:24:50 +0100

    Component Created for Shows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,6 @@ import React from 'react';
 import './App.css';
 import Shows from './Components/Shows';
 
-// const Shows = () =>{
-//   const [shows,setshows] = useState([]);
-
-
-
-
-//   const fetchShows = () => {
-//     axios.get("http://localhost:8080/shows").then(res =>{
-//       console.log(res);
-
-//       setshows (res.data);
-
-
-//     });
-//   };
-
-//   useEffect(() => {
-//     fetchShows();
-//   }, []);
-
-//   return shows.map((shows, index) => {
-
-//     return (
-//       <div key= {index}>
-//         <h1>{shows.name}</h1>
-//         <p>{shows.genres}</p>
-//         <h1>{shows.rat}</h1>
-//       </div>
-//     );
-//   });
-
-// };
 
 function App() {
   return (
