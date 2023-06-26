2e0e26e9531d13818780de31bf37aa8dc3c35399
Mabroor Ahmed <mabroorahmed18@gmail.com>
2023-06-26 01:13:31 +0100

    Show Image return with Name and Genres

--- springboot/src/main/frontend/src/App.js
+++ springboot/src/main/frontend/src/App.js
@@ -1,43 +1,46 @@
-import React, {useState,useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
+import Shows from './Components/Shows';
 
-const Shows = () =>{
-  const [shows,setshows] = useState([]);
-
-
+// const Shows = () =>{
+//   const [shows,setshows] = useState([]);
 
 
-  const fetchShows = () => {
-    axios.get("http://localhost:8080/shows").then(res =>{
-      console.log(res);
-
-      setshows (res.data);
 
 
-    });
-  };
+//   const fetchShows = () => {
+//     axios.get("http://localhost:8080/shows").then(res =>{
+//       console.log(res);
 
-  useEffect(() => {
-    fetchShows();
-  }, []);
+//       setshows (res.data);
 
-  return shows.map((shows, index) => {
 
-    return (
-      <div key= {index}>
-        <h1>{shows.name}</h1>
-        <p>{shows.genres}</p>
-      </div>
-    );
-  });
+//     });
+//   };
 
-};
+//   useEffect(() => {
+//     fetchShows();
+//   }, []);
+
+//   return shows.map((shows, index) => {
+
+//     return (
+//       <div key= {index}>
+//         <h1>{shows.name}</h1>
+//         <p>{shows.genres}</p>
+//         <h1>{shows.rat}</h1>
+//       </div>
+//     );
+//   });
+
+// };
 
 function App() {
   return (
-    <div className="App">
+    <div >
     <Shows/>
+
+
     </div>
   );
 }
--- springboot/src/main/frontend/src/Components/Shows.js
+++ springboot/src/main/frontend/src/Components/Shows.js
@@ -1,5 +1,36 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+function Shows() {
+    
+    const [shows,setshows] = useState([]);
+    const fetchShows = () => {
+        axios.get("http://localhost:8080/shows").then(res =>{
+        console.log(res);
+
+        setshows (res.data);
 
 
+        });
+    };
 
+    useEffect(() => {
+        fetchShows();
+    }, []);
 
-  
+    return shows.map((shows, index) => {
+
+        return (
+        <div key= {index}>
+            <img src ={shows.image.medium} alt ={shows.name+"picture"} /> 
+            <h1>{shows.name}</h1>
+            <p>{"Genres ["+shows.genres+"]"}</p>
+            
+        </div>
+        );
+    });
+
+    
+};
+
+export default Shows;
